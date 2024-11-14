05eac2eddbf8dbc2fa228079e5a0ed36d08a0bf5
MANAS RATHORE <84979015+Manasthor@users.noreply.github.com>
2024-11-14 10:43:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function insert_Row() {
-    var table = document.getElementById("sampleTable")
-	var newRow = table.insert_Row(0)
-	var cell1 = newRow.insertCell(0);
+    var table = document.getElementById("sampleTable");
+    
+    var newRow = table.insertRow(0);
+    
+    var cell1 = newRow.insertCell(0);
     var cell2 = newRow.insertCell(1);
-	cell1.innerHTML="New Cell1"
-	cell2.innerHTML="New Cell2"
+    
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
 }
